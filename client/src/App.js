dff3acd86bba7da6e38026f1ba4889d69fe77008
Robert Pierce <pierce1673@gmail.com>
2020-01-09 14:23:06 -0500

    displaying 'saved results' now

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Search from "./components/Search";
 import NavBar from "./components/navBar";
+import PreviousResults from "./components/PreviousResults";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
           <span className="xbox">for XBOX</span>
         </h1>
         <Search />
+      </Wrapper>
+      <Wrapper>
+        <PreviousResults />
       </Wrapper>
     </div>
   );
--- client/src/components/UserGreeting.js
+++ client/src/components/UserGreeting.js
@@ -1,7 +1,32 @@
 import React from "react";
+import API from "../utils/API.js";
+import Button from "@material-ui/core/Button";
 
-function UserGreeting(props) {
-  return <h3>&nbsp;&nbsp;Welcome {props.username}!</h3>;
+class UserGreeting extends React.Component {
+  state = {};
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.logout().then(answer => {
+      console.log(answer);
+      if (answer.data.logout == true) {
+        this.props.handleLogin(false, "");
+      }
+    });
+  };
+
+  // componentDidMount(){
+  //   this.setState({ username: this.props.username})
+  // }
+
+  render() {
+    return (
+      <>
+        <h3>&nbsp;&nbsp;Welcome {this.props.username}!</h3>
+        <Button onClick={this.handleFormSubmit}>Logout</Button>
+      </>
+    );
+  }
 }
 
 export default UserGreeting;

27dec3c3152e2011c6594e66aa6b470957650479
Robert Pierce <pierce1673@gmail.com>
2020-01-09 15:51:36 -0500

    triston's updates + removing commented out stuff

--- routes/xboxMarketScrape.js
+++ routes/xboxMarketScrape.js
@@ -1,43 +1,6 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-/*module.exports = function scrapeMarket(searchTerm, res) {
-  const encodedSearch = encodeURI(searchTerm);
-
-  axios
-    .get(`https://www.microsoft.com/en-us/search/shop/games?q=${encodedSearch}`)
-    .then(function(response) {
-      var $ = cheerio.load(response.data);
-
-      const result = {};
-
-      result.title = $("#coreui-productplacementlist-1g76zxk_0")
-        .children("h3")
-        .text();
-
-      result.price = $("#coreui-productplacementlist-1g76zxk_0")
-        .children("div.c-channel-placement-price")
-        .children("div")
-        .children("span")
-        .text();
-
-      result.image = $("#coreui-productplacementlist-1g76zxk")
-        .children("div:nth-child(3)")
-        .children("div.c-group.f-wrap-items.context-list-page")
-        .children("div:nth-child(1)")
-        .children("a")
-        .children("div.c-channel-placement-image")
-        .children("picture")
-        .children("img")
-        .attr("data-src");
-
-      var remove = result.price.trim(" ");
-
-      console.log(remove);
-      res.json(result);
-    });
-};*/
-
 module.exports = function scrapeMarket(searchTerm, res) {
   const encodedSearch = encodeURI(searchTerm);
 
